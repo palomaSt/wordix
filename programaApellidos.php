69f997c147e1588f3cf4647483f2634aeabefec2
palomaStucke <paloma.stucke@est.fi.uncoma.edu.ar>
2024-11-20 17:35:06 -0300

    revise y complete mis datos en programa

---
+++ programaApellidos.php
@@ -0,0 +1,80 @@
+<?php
+include_once("wordix.php");
+
+
+
+/**************************************/
+/***** DATOS DE LOS INTEGRANTES *******/
+/**************************************/
+
+/* Stucke, Paloma. FAI-5544 . Tecnicatura en Desarrollo Web. paloma.stucke@est.fi.uncoma.edu.ar . palomaStucke */
+/* Apellido, Nombre. Legajo. Carrera. mail. Usuario Github */
+/* ****COMPLETAR***** */
+
+
+/**************************************/
+/***** DEFINICION DE FUNCIONES ********/
+/**************************************/
+
+/**
+ * Obtiene una colección de palabras
+ * @return array
+ */
+function cargarColeccionPalabras()
+{
+    $coleccionPalabras = [
+        "MUJER", "QUESO", "FUEGO", "CASAS", "RASGO",
+        "GATOS", "GOTAS", "HUEVO", "TINTO", "NAVES",
+        "VERDE", "MELON", "YUYOS", "PIANO", "PISOS",
+        "ALETA", "CALMA", "ZEBRA", "SIGNO", "RULOS"
+        
+    ];
+
+    return ($coleccionPalabras);
+}
+
+/* ****COMPLETAR***** */
+
+
+
+/**************************************/
+/*********** PROGRAMA PRINCIPAL *******/
+/**************************************/
+
+//Declaración de variables:
+
+
+//Inicialización de variables:
+
+
+//Proceso:
+
+$partida = jugarWordix("MELON", strtolower("MaJo"));
+//print_r($partida);
+//imprimirResultado($partida);
+
+
+
+/*
+do {
+    $opcion = ...;
+
+    
+    switch ($opcion) {
+        case 1: 
+            //completar qué secuencia de pasos ejecutar si el usuario elige la opción 1
+
+            break;
+        case 2: 
+            //completar qué secuencia de pasos ejecutar si el usuario elige la opción 2
+
+            break;
+        case 3: 
+            //completar qué secuencia de pasos ejecutar si el usuario elige la opción 3
+
+            break;
+        
+            //...
+    }
+} while ($opcion != X);
+*/
